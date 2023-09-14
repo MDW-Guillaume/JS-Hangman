0231ce638448dea4ac7c5907dd70d3b476e41691
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2023-09-14 14:05:48 +0200

    ADD Fields to fill display

--- app.js
+++ app.js
@@ -5,4 +5,29 @@ const inputSystem = new Input();
 const provider = new Provider();
 
 inputSystem.init()
-console.log(provider.getOneWord())
+
+const oneWord = provider.getOneWord()
+
+function generateCompletionArray(oneWord){
+
+    let wordArray = new Array(oneWord.length).fill('_');
+
+    let enteriesContainer = document.getElementById('enteries');
+
+    // Crée les éléments div avec la classe "enteries__element" et le contenu '<span>_</span>'
+    for (let i = 0; i < wordArray.length; i++) {
+        const elementDiv = document.createElement('div');
+        elementDiv.classList.add('enteries__element');
+    
+        const spanElement = document.createElement('span');
+        spanElement.textContent = '_';
+    
+        elementDiv.appendChild(spanElement);
+        enteriesContainer.appendChild(elementDiv);
+    }
+    
+    return wordArray;
+}
+
+console.log(oneWord)
+generateCompletionArray(oneWord)
---
+++ doc.md
@@ -0,0 +1,9 @@
+# Projet pendu
+
+## Besoins 
+
+Providers ( mots ) -> Plusieurs difficultées  ( mots plus ou moins longs selon la diff )
+
+Class Pendu -> gérer les lettre déjà ditent
+
+Class Input
