4770c7ffe03dfd092fb19de17a0c0b1c636e5331
MDW-Guillaume <guillaume.couvidou@mozartsduweb.com>
2023-10-19 10:48:45 +0200

    Update leaderboard system

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -15,30 +15,46 @@ export default class Leaderboard {
         this.leaderboard[playerName] = score;
         this.manager.setLeaderboard(this.leaderboard);
     }
-
+    
     renderLeaderboard() {
+        this.sortLeaderboard()
         let remode = document.querySelectorAll('.leaderboard__element');
 
         remode.forEach((el) => {
             el.remove();
         })
 
-        Object.entries(this.leaderboard).forEach((infos) => {
-            console.log(infos);
+        let placement = 1;
 
+        Object.entries(this.leaderboard).sort().forEach((infos) => {
             const elementDiv = document.createElement('tr');
             elementDiv.classList.add('leaderboard__element');
         
             const spanElement = document.createElement('td');
-            spanElement.textContent = infos[0];
-        
+            spanElement.textContent = placement;
+
             const spanElement2 = document.createElement('td');
-            spanElement2.textContent = infos[1];
+            spanElement2.textContent = infos[0];
+        
+            const spanElement3 = document.createElement('td');
+            spanElement3.textContent = infos[1];
         
             elementDiv.appendChild(spanElement);
             elementDiv.appendChild(spanElement2);
+            elementDiv.appendChild(spanElement3);
         
             this.leaderboardContainer.appendChild(elementDiv);
+            placement++;
+        })
+    }
+
+    sortLeaderboard(){
+        const sorted = Object.entries(this.leaderboard).sort((a, b) => b[1] - a[1]);
+
+        this.leaderboard = {};
+
+        sorted.forEach((infos) => {
+            this.leaderboard[infos[0]] = infos[1];
         })
     }
 }
